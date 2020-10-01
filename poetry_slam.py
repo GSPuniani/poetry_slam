c4455414797967eb81daaed5f00b0bd5c8777af5
Gobind Puniani <gobindpuniani@gmail.com>
2020-10-01 01:51:40 -0700

    Finished third function and started fourth function

--- poetry_slam.py
+++ poetry_slam.py
@@ -37,10 +37,49 @@ def lines_printed_backwards(lines_list):
     return backwards_poem
 
 
+def lines_printed_random(lines_list):
+    """
+    Takes in a list of lines of a file and returns the lines in random order 
+    using the `randint()` method from the `random` library.
+    Input: a list of strings
+    Output: a string
+    """
+    # Define a new string variable to hold all of the lines in one string
+    backwards_poem = ""
+    # Assign the length of the reversed list to a variable for easy access
+    list_length = len(lines_list)
+    # Iterate through the reversed list
+    for i in range(list_length):
+        # Assign `i` to another variable to avoid an IndexError upon adding 1 in the f-string
+        line_counter = i
+        # Add a line number and a random line to the output string by calling an element with a random index
+        backwards_poem += f"{line_counter + 1} {lines_list[random.randint(0, list_length)]}"
+    return backwards_poem
 
+
+# Stretch Challenge #4
+def lines_printed_custom(lines_list):
+    """
+    Takes in a list of lines of a file and returns the words on each line in random order 
+    using the `randint()` method from the `random` library.
+    Input: a list of strings
+    Output: a string
+    """
+    # Define a new string variable to hold all of the lines in one string
+    backwards_poem = ""
+    # Assign the length of the reversed list to a variable for easy access
+    list_length = len(lines_list)
+    # Iterate through the reversed list
+    for i in range(list_length):
+        # Assign `i` to another variable to avoid an IndexError upon adding 1 in the f-string
+        line_counter = i
+        # Add a line number and a random line to the output string by calling an element with a random index
+        backwards_poem += f"{line_counter + 1} {lines_list[random.randint(0, list_length)]}"
+    return backwards_poem
 
 
 # Assign the list of lines from the poem to a new variable
 poem_lines_list = get_file_lines("interstellar_poem.txt")
 # print(poem_lines_list)
 # print(lines_printed_backwards(poem_lines_list))
+print(lines_printed_random(poem_lines_list))
